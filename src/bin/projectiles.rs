313f1bc76535afc61cc39296f63454d8dfa7fd38
Emily Fox <kylejfox@gmail.com>
2023-04-17 17:10:26 -0500

    Plot path of projectile

--- src/bin/projectiles.rs
+++ src/bin/projectiles.rs
@@ -1,4 +1,9 @@
-use ray_tracer_challenge::{Point, Vector};
+use std::{fs::File, io::Write};
+
+use ray_tracer_challenge::{
+    canvas::{Canvas, Color},
+    Point, Vector,
+};
 
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
 struct Projectile {
@@ -30,17 +35,31 @@ fn tick(environment: Environment, projectile: Projectile) -> Projectile {
     Projectile::new(position, velocity)
 }
 
-fn main() {
-    let mut projectile = Projectile::new(
-        Point::new(0.0, 1.0, 0.0),
-        Vector::new(1.0, 1.0, 0.0).normalize(),
-    );
-    let environment = Environment::new(Vector::new(0.0, -0.1, 0.0), Vector::new(-0.01, 0.0, 0.0));
+fn main() -> std::io::Result<()> {
+    let start = Point::new(0.0, 1.0, 0.0);
+    let velocity = Vector::new(1.0, 1.8, 0.0).normalize() * 11.25;
+    let mut projectile = Projectile::new(start, velocity);
+
+    let gravity = Vector::new(0.0, -0.1, 0.0);
+    let wind = Vector::new(-0.01, 0.0, 0.0);
+    let environment = Environment::new(gravity, wind);
+
+    let mut canvas = Canvas::new(900, 550);
 
     let mut num_ticks = 0;
+    _ = canvas.write_pixel(
+        projectile.position.x() as usize,
+        549 - projectile.position.y() as usize,
+        Color::new(0.75, 0.3, 0.0),
+    );
     while projectile.position.y() > 0.0 {
         projectile = tick(environment, projectile);
         num_ticks += 1;
+        _ = canvas.write_pixel(
+            projectile.position.x() as usize,
+            549 - projectile.position.y() as usize,
+            Color::new(0.75, 0.3, 0.0),
+        );
         println!(
             "Projectile at ({:.2}, {:.2}, {:.2}) after {} tick(s).",
             projectile.position.x(),
@@ -52,4 +71,12 @@ fn main() {
 
     println!();
     println!("Projectile took {num_ticks} ticks to hit the ground!");
+
+    let output_path = std::env::args().nth(1);
+    if let Some(output_path) = output_path {
+        let mut file = File::create(output_path)?;
+        file.write_all(canvas.to_ppm().as_bytes())?;
+    }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod canvas;
+pub mod canvas;
 mod tuples;
 
 pub use tuples::{Point, Vector};
