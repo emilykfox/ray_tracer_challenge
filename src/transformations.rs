364d2418210fdc70019025dd2d4f15cf36b4ac7e
Emily Fox <kylejfox@gmail.com>
2023-04-20 12:00:39 -0500

    Add scaling transformations

--- src/transformations.rs
+++ src/transformations.rs
@@ -1,10 +1,19 @@
-use crate::matrices::{Matrix, IDENTITY};
+use crate::matrices::Matrix;
 
 pub fn translation(x: f64, y: f64, z: f64) -> Matrix {
     Matrix::new([
         [1.0, 0.0, 0.0, x],
         [0.0, 1.0, 0.0, y],
         [0.0, 0.0, 1.0, z],
+        [0.0, 0.0, 0.0, 1.0],
+    ])
+}
+
+pub fn scaling(x: f64, y: f64, z: f64) -> Matrix {
+    Matrix::new([
+        [x, 0.0, 0.0, 0.0],
+        [0.0, y, 0.0, 0.0],
+        [0.0, 0.0, z, 0.0],
         [0.0, 0.0, 0.0, 1.0],
     ])
 }
@@ -34,5 +43,27 @@ mod test {
         let transform = translation(5.0, -3.0, 2.0);
         let v = Vector::new(-3.0, 4.0, 5.0);
         assert_eq!(transform * v, Ok(v));
+    }
+
+    #[test]
+    fn scale_point() {
+        let transform = scaling(2.0, 3.0, 4.0);
+        let p = Point::new(-4.0, 6.0, 8.0);
+        assert_eq!(transform * p, Ok(Point::new(-8.0, 18.0, 32.0)));
+    }
+
+    #[test]
+    fn scale_vector() {
+        let transform = scaling(2.0, 3.0, 4.0);
+        let v = Vector::new(-4.0, 6.0, 8.0);
+        assert_eq!(transform * v, Ok(Vector::new(-8.0, 18.0, 32.0)));
+    }
+
+    #[test]
+    fn inverse_scale() {
+        let transform = scaling(2.0, 3.0, 4.0);
+        let inverse = transform.inverse().expect("cannot take inverse");
+        let v = Vector::new(-4.0, 6.0, 8.0);
+        assert_eq!(inverse * v, Ok(Vector::new(-2.0, 2.0, 2.0)));
     }
 }
