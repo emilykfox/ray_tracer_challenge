0a9cee91aa052b4c705fc0e204789bb337e73346
Emily Fox <kylejfox@gmail.com>
2023-04-21 09:54:32 -0500

    Sphere::intersect now returns list of intersection objects

--- src/spheres.rs
+++ src/spheres.rs
@@ -1,4 +1,8 @@
-use crate::{rays::Ray, Point, Vector};
+use crate::{
+    intersections::{Intersection, Intersections},
+    rays::Ray,
+    Point, Vector,
+};
 
 #[derive(Default, Debug, Copy, Clone, PartialEq)]
 pub struct Sphere {}
@@ -8,7 +12,7 @@ impl Sphere {
         Sphere {}
     }
 
-    pub fn intersect(&self, ray: Ray) -> Vec<f64> {
+    pub fn intersect(&self, ray: Ray) -> Intersections {
         let origin = ray.origin();
         let direction = ray.direction();
         let sphere_to_ray = origin - Point::new(0.0, 0.0, 0.0);
@@ -20,11 +24,14 @@ impl Sphere {
         let discriminant = b * b - 4.0 * a * c;
 
         if discriminant < 0.0 {
-            Vec::new()
+            Intersections::new(vec![])
         } else {
             let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
             let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
-            vec![t1, t2]
+            Intersections::new(vec![
+                Intersection::new(t1, self),
+                Intersection::new(t2, self),
+            ])
         }
     }
 }
@@ -41,8 +48,8 @@ mod test {
         let s = Sphere::new();
         let xs = s.intersect(r);
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], 4.0);
-        assert_eq!(xs[1], 6.0);
+        assert_eq!(xs[0].t(), 4.0);
+        assert_eq!(xs[1].t(), 6.0);
     }
 
     #[test]
@@ -51,8 +58,8 @@ mod test {
         let s = Sphere::new();
         let xs = s.intersect(r);
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], 5.0);
-        assert_eq!(xs[1], 5.0);
+        assert_eq!(xs[0].t(), 5.0);
+        assert_eq!(xs[1].t(), 5.0);
     }
 
     #[test]
@@ -69,8 +76,8 @@ mod test {
         let s = Sphere::new();
         let xs = s.intersect(r);
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], -1.0);
-        assert_eq!(xs[1], 1.0);
+        assert_eq!(xs[0].t(), -1.0);
+        assert_eq!(xs[1].t(), 1.0);
     }
 
     #[test]
@@ -79,7 +86,17 @@ mod test {
         let s = Sphere::new();
         let xs = s.intersect(r);
         assert_eq!(xs.len(), 2);
-        assert_eq!(xs[0], -6.0);
-        assert_eq!(xs[1], -4.0);
+        assert_eq!(xs[0].t(), -6.0);
+        assert_eq!(xs[1].t(), -4.0);
+    }
+
+    #[test]
+    fn intersection_sets_object() {
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new();
+        let xs = s.intersect(r);
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].object(), &s);
+        assert_eq!(xs[1].object(), &s);
     }
 }
