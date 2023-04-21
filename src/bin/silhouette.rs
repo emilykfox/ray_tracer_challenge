8c452c09938fa36049fff40afc57ef351fe4083d
Emily Fox <kylejfox@gmail.com>
2023-04-21 17:28:41 -0500

    Follow book's silhouette approach

--- src/bin/silhouette.rs
+++ src/bin/silhouette.rs
@@ -2,35 +2,33 @@ use ray_tracer_challenge::{
     canvas::{Canvas, Color},
     rays::Ray,
     spheres::Sphere,
-    transformations::translation,
-    Point, Vector,
+    Point,
 };
 
 fn main() -> std::io::Result<()> {
-    let mut canvas = Canvas::new(400, 400);
-    let hit_color = Color::new(1.0, 0.0, 0.0);
+    let ray_origin = Point::new(0.0, 0.0, -5.0);
+    let wall_z = 10.0;
+    let wall_size = 7.0;
 
-    let transform = translation(0.0, 0.0, 5.0);
-    let mut sphere = Sphere::new();
-    sphere.set_transform(transform);
+    let canvas_pixels = 100;
+    let pixel_size = wall_size / canvas_pixels as f64;
+    let half = wall_size / 2.0;
 
-    let direction = Vector::new(0.0, 0.0, 1.0);
+    let mut canvas = Canvas::new(canvas_pixels, canvas_pixels);
+    let color = Color::new(1.0, 0.0, 0.0);
+    let shape = Sphere::new();
 
-    for x in 0..400 {
-        for y in 0..400 {
-            let ray = Ray::new(
-                Point::new(
-                    1.3 * (x as f64 / 200.0 - 1.0),
-                    1.3 * (-(y as f64 / 200.0) + 1.0),
-                    0.0,
-                ),
-                direction,
-            );
-            let intersections = sphere.intersect(ray).unwrap();
+    for y in 0..canvas_pixels {
+        let world_y = half - pixel_size * y as f64;
+        for x in 0..canvas_pixels {
+            let world_x = -half + pixel_size * x as f64;
+            let position = Point::new(world_x, world_y, wall_z);
+            let ray = Ray::new(ray_origin, (position - ray_origin).normalize());
+            let intersections = shape.intersect(ray).unwrap();
             if intersections.hit().is_some() {
                 canvas
-                    .write_pixel(x, y, hit_color)
-                    .expect("cannot write pixel");
+                    .write_pixel(x, y, color)
+                    .expect("unable to write pixel");
             }
         }
     }
