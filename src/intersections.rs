52c4ef02e23ed9e212e544dfe02e2c642d187341
Emily Fox <kylejfox@gmail.com>
2023-04-21 10:29:56 -0500

    Add first hits

--- src/intersections.rs
+++ src/intersections.rs
@@ -33,6 +33,14 @@ impl<'objects> Intersections<'objects> {
     pub fn len(&self) -> usize {
         self.intersections.len()
     }
+
+    pub fn hit(&self) -> Option<Intersection<'objects>> {
+        self.intersections
+            .iter()
+            .filter(|intersection| intersection.t() >= 0.0)
+            .min_by(|x, y| x.t().total_cmp(&y.t()))
+            .copied()
+    }
 }
 
 impl<'objects> std::ops::Index<usize> for Intersections<'objects> {
@@ -66,5 +74,47 @@ mod test {
         assert_eq!(xs.len(), 2);
         assert_eq!(xs[0].t(), 1.0);
         assert_eq!(xs[1].t(), 2.0);
+    }
+
+    #[test]
+    fn hit_all_positive() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(1.0, &s);
+        let i2 = Intersection::new(2.0, &s);
+        let xs = Intersections::new(vec![i2, i1]);
+        let i = xs.hit();
+        assert_eq!(i, Some(i1));
+    }
+
+    #[test]
+    fn hit_some_negative() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-1.0, &s);
+        let i2 = Intersection::new(1.0, &s);
+        let xs = Intersections::new(vec![i2, i1]);
+        let i = xs.hit();
+        assert_eq!(i, Some(i2));
+    }
+
+    #[test]
+    fn hit_all_negative() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-2.0, &s);
+        let i2 = Intersection::new(-1.0, &s);
+        let xs = Intersections::new(vec![i2, i1]);
+        let i = xs.hit();
+        assert_eq!(i, None);
+    }
+
+    #[test]
+    fn hit_lowest_nonnegative() {
+        let s = Sphere::new();
+        let i1 = Intersection::new(5.0, &s);
+        let i2 = Intersection::new(7.0, &s);
+        let i3 = Intersection::new(-3.0, &s);
+        let i4 = Intersection::new(2.0, &s);
+        let xs = Intersections::new(vec![i1, i2, i3, i4]);
+        let i = xs.hit();
+        assert_eq!(i, Some(i4));
     }
 }
