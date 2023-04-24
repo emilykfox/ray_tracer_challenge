2e562ff7aa17925564d597d680dd2fd4d0230e65
Emily Fox <kylejfox@gmail.com>
2023-04-24 18:14:31 -0500

    Make default camera for real

---
+++ src/camera.rs
@@ -0,0 +1,39 @@
+use crate::matrices::Transform;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Camera {
+    pub hsize: usize,
+    pub vsize: usize,
+    pub field_of_view: f64,
+    transform: Transform,
+}
+
+impl Camera {
+    pub fn new(hsize: usize, vsize: usize, field_of_view: f64) -> Self {
+        Camera {
+            hsize,
+            vsize,
+            field_of_view,
+            transform: Transform::identity(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::f64::consts::PI;
+
+    use super::*;
+
+    #[test]
+    fn construct_camera() {
+        let hsize = 160;
+        let vsize = 120;
+        let field_of_view = PI / 2.0;
+        let c = Camera::new(hsize, vsize, field_of_view);
+        assert_eq!(c.hsize, 160);
+        assert_eq!(c.vsize, 120);
+        assert_eq!(c.field_of_view, PI / 2.0);
+        assert_eq!(c.transform, Transform::identity());
+    }
+}
