1ced22762cee6cd49b04b029f9a32a00802a522b
Emily Fox <kylejfox@gmail.com>
2023-04-24 20:56:26 -0500

    Compute pixel size

--- src/camera.rs
+++ src/camera.rs
@@ -2,19 +2,40 @@ use crate::matrices::Transform;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Camera {
-    pub hsize: usize,
-    pub vsize: usize,
-    pub field_of_view: f64,
+    hsize: usize,
+    vsize: usize,
+    field_of_view: f64,
     transform: Transform,
+    half_width: f64,
+    half_height: f64,
+    pixel_size: f64,
 }
 
 impl Camera {
     pub fn new(hsize: usize, vsize: usize, field_of_view: f64) -> Self {
+        let half_view = (field_of_view / 2.0).tan();
+        let aspect = hsize as f64 / vsize as f64;
+
+        let half_width;
+        let half_height;
+        if aspect >= 1.0 {
+            half_width = half_view;
+            half_height = half_view / aspect;
+        } else {
+            half_width = half_view * aspect;
+            half_height = half_view;
+        }
+
+        let pixel_size = (half_width * 2.0) / hsize as f64;
+
         Camera {
             hsize,
             vsize,
             field_of_view,
             transform: Transform::identity(),
+            half_width,
+            half_height,
+            pixel_size,
         }
     }
 }
@@ -22,6 +43,8 @@ impl Camera {
 #[cfg(test)]
 mod test {
     use std::f64::consts::PI;
+
+    use crate::EQUALITY_EPSILON;
 
     use super::*;
 
@@ -35,5 +58,17 @@ mod test {
         assert_eq!(c.vsize, 120);
         assert_eq!(c.field_of_view, PI / 2.0);
         assert_eq!(c.transform, Transform::identity());
+    }
+
+    #[test]
+    fn pixel_size_for_horizontal() {
+        let c = Camera::new(200, 125, PI / 2.0);
+        assert!((c.pixel_size - 0.01).abs() < EQUALITY_EPSILON);
+    }
+
+    #[test]
+    fn pixel_size_for_vertical() {
+        let c = Camera::new(125, 200, PI / 2.0);
+        assert!((c.pixel_size - 0.01).abs() < EQUALITY_EPSILON);
     }
 }
