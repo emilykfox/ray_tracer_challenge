8210b8cc8621b62d3188d22df4f5fc5f1877ada3
Emily Fox <kylejfox@gmail.com>
2023-05-09 12:08:28 -0500

    Start intersections refactor

--- src/intersections.rs
+++ src/intersections.rs
@@ -38,6 +38,14 @@ impl<'objects> Intersections<'objects> {
     }
 }
 
+impl<'objects> std::ops::Deref for Intersections<'objects> {
+    type Target = Vec<Intersection<'objects>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.vec
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct HitInfo<'object> {
     pub t: f64,
@@ -82,7 +90,7 @@ mod test {
     use crate::{
         rays::Ray,
         shapes::{Plane, Sphere},
-        transformations::translation,
+        transformations::{scaling, translation},
         Point, Vector,
     };
 
@@ -207,5 +215,43 @@ mod test {
             hit_info.reflectv,
             Vector::new(0.0, 2_f64.sqrt() / 2.0, 2_f64.sqrt() / 2.0)
         );
+    }
+
+    #[test]
+    fn various_n1_and_n2() {
+        let mut a = Sphere::new_glass();
+        a.set_transform(scaling(2.0, 2.0, 2.0));
+        a.material.refractive_index = 1.5;
+        let mut b = Sphere::new_glass();
+        b.set_transform(translation(0.0, 0.0, -0.25));
+        b.material.refractive_index = 2.0;
+        let mut c = Sphere::new_glass();
+        c.set_transform(translation(0.0, 0.0, 0.25));
+        c.material.refractive_index = 2.5;
+
+        let r = Ray::new(Point::new(0.0, 0.0, -4.0), Vector::new(0.0, 0.0, 1.0));
+        let xs = Intersections::new(vec![
+            Intersection::new(2.0, &a),
+            Intersection::new(2.75, &b),
+            Intersection::new(3.25, &c),
+            Intersection::new(4.75, &b),
+            Intersection::new(5.25, &c),
+            Intersection::new(6.0, &a),
+        ]);
+
+        let examples = vec![
+            (1.0, 1.5),
+            (1.5, 2.0),
+            (2.0, 2.5),
+            (2.5, 2.5),
+            (2.5, 1.5),
+            (1.5, 1.0),
+        ];
+        for (index, pair) in examples.iter().enumerate() {
+            let hit_info = HitInfo::prepare(&xs.vec[index], &r);
+            todo!();
+            //TODO: Finish test assert_eq!(hit_info.n1, pair.0);
+            //assert_eq!(hit_info.n2, pair.1);
+        }
     }
 }
